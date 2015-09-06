9d3f804d0bb4b31b2033cf922ce0e4297842da3a
Seapagan <seapagan@gmail.com>
2015-09-06 07:10:17 +0100

    Add 'SimpleCov' gem as developemnt dependency
    Signed-off-by: Seapagan <seapagan@gmail.com>

--- optioneer.gemspec
+++ optioneer.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'coveralls'
   spec.add_development_dependency 'rubocop'
   spec.add_development_dependency 'inch'
+  spec.add_development_dependency 'simplecov', '~> 0.10'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
 
 require 'optioneer'
