af8dad650734e1856e923d7d232e6c75371395f5
Grant Ramsay <seapagan@gmail.com>
2015-09-30 09:02:06 +0100

    Adjust tests after converting to module
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- spec/optioneer_spec.rb
+++ spec/optioneer_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe Optioneer do
+  subject { Optioneer::Optioneer }
   it 'has a version number' do
     expect(Optioneer::VERSION).not_to be nil
   end
 
+  let(:options) { subject.new }
   it 'should create an instance of the class with no errors' do
-    expect(subject).to be_a Optioneer
+    expect(options).to be_an_instance_of subject
+  end
+  context 'adding new command line options' do
+
   end
 end
