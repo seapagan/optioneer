1b1fdd8e5b42f1f16ca5fffe2734b90ce16d9682
Grant Ramsay <seapagan@gmail.com>
2015-09-30 15:53:38 +0100

    Tidy up tests
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- spec/optioneer_spec.rb
+++ spec/optioneer_spec.rb
@@ -5,12 +5,9 @@ describe Optioneer do
   it 'has a version number' do
     expect(Optioneer::VERSION).not_to be nil
   end
-
-  let(:options) { subject.new }
-  it 'should create an instance of the class with no errors' do
-    expect(options).to be_an_instance_of subject
+  it 'creates an instance of the class with no errors' do
+    expect(subject.new).to be_an_instance_of subject
   end
   context 'adding new command line options' do
-
   end
 end
