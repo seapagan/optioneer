877366310e01dd536b8e2ec87a588c6ceb147984
Grant Ramsay <seapagan@gmail.com>
2015-09-30 08:41:00 +0100

    Wrap entire Gem in a Module.
    Keep namespace clean.
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lib/optioneer.rb
+++ lib/optioneer.rb
@@ -1,17 +1,21 @@
 require 'optioneer/version'
+require 'optioneer/option'
 
 # A self-contained interface to the command line
-class Optioneer
-  # Hash containin any options we want to define as defaults of not specified
-  DEFAULT_OPTIONS = {}
+module Optioneer
+  # Main control class
+  class Optioneer
+    # Hash containin any options we want to define as defaults of not specified
+    DEFAULT_OPTIONS = {}
 
-  # Setup the class with specified parameters or default values if any or all
-  # are absent.
-  # All parameters are optional.
-  # @param options [Hash] Hash value containing any passed parameters.
-  def initialize(options = {})
-    @options = DEFAULT_OPTIONS.merge(options)
-    # set all other unset options to return false instead of Nul.
-    @options.default = false
+    # Setup the class with specified parameters or default values if any or all
+    # are absent.
+    # All parameters are optional.
+    # @param options [Hash] Hash value containing any passed parameters.
+    def initialize(options = {})
+      @options = DEFAULT_OPTIONS.merge(options)
+      # set all other unset options to return false instead of Nul.
+      @options.default = false
+    end
   end
 end
