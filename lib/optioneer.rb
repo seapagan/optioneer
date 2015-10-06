1fb1b54996a1d61da610fc9fa1e068de8000f400
Grant Ramsay <seapagan@gmail.com>
2015-10-06 19:21:25 +0100

    Add first part of command line parsing.
    In progress, still a few edge cases to test for and fix.
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lib/optioneer.rb
+++ lib/optioneer.rb
@@ -17,40 +17,118 @@ module Optioneer
       # set all other unset options to return false instead of Nul.
       @options.default = false
 
-      @options[:cmdline] = ARGV
+      # save the comand line passed to the program.
+      @options[:cmdline] = ARGV.clone
+      # save a tweaked version for internal usage.
+      @options[:cmd_tweaked] = tweak_cmd(ARGV)
 
-      @command_options = []
+      # array to hold the EXPECTED comamnd line options
+      @expected_options = []
+      # hash to hold the named PASSED command line options
+      @passed_options = {}
     end
 
     # Start parsing the command line, return it in a format that is easy to use.
     def parse
-      p ARGV
+      @options[:cmd_tweaked].each_with_index do |opt, index|
+        if opt.match(/^-/)
+          # found a switch, is it long?
+          if opt.match(/^--/)
+            opt.gsub!(/^--/, '')
+            find_option(opt, :long)
+          else # no it must be short
+            opt.gsub!(/^-/, '')
+            find_option(opt, :short)
+          end
+        # if we are not a switch, must be an action, but only if last item.
+        elsif index == @options[:cmd_tweaked].count - 1
+          @options[:action] = opt
+        end
+      end
+      @passed_options.count
     end
 
+    def action
+      @options[:action]
+    end
+
+    # allow setting of the help display banner
+    def banner=(banner)
+      @options[:banner] = banner
+    end
+
+    # return the current help display banner
+    def banner
+      @options[:banner]
+    end
+
+    # returns the original un-parsed command line passed to the program
     def cmdline
       @options[:cmdline]
     end
 
     # Add a new option to the list, assuming it does not already exist.
-    def add(name)
+    def add(name, options = {})
       unless find_opt_by_name(name)
-        new_option = Option.new(name)
-        @command_options.push(new_option)
+        new_option = Option.new(name, options)
+        @expected_options.push(new_option)
         return new_option
       end
       fail 'You Cannot create 2 options with the same name.'
     end
 
+    # return the named passed option
+    def [](name)
+      @passed_options[name]
+    end
+
+    # return the count of EXPECTED options
     def count
-      @command_options.count
+      @expected_options.count
     end
 
     private
 
-    # Given a name, will return details of the matching Option.
+    # adjust the command line to lump any short-form arguments together
+    def tweak_cmd(cmd_line)
+      cmd_line.each_with_index do |arg, index|
+        next unless arg.match(/^-[A-Za-z0-9]$/)
+        # we have a short form, see if there is an argument next in the array
+        next if cmd_line[index + 1].match(/^-/)
+        # there is so we merge the 2 parts, with '=' between them
+        cmd_line[index] = "#{cmd_line[index]}=#{cmd_line[index + 1]}"
+        cmd_line.delete_at(index + 1)
+      end
+    end
+
+    # searches for a match in the expected options.
+    def find_option(opt, which)
+      # loop through all options and see if we have a match
+      @expected_options.each do |expected|
+        # we need to test first if an argument is included...
+        the_opt = opt
+        the_opt, the_arg = opt.split('=') if opt =~ /\=/
+        next unless the_opt == expected.values[which]
+        new_option = Option.new(expected.name)
+        which == :long ? new_option.long = the_opt : new_option.short = the_opt
+        new_option.argument = the_arg
+        # check for both short and long options and fail if so
+        check_duplicate(expected)
+        @passed_options[expected.name] = new_option
+      end
+    end
+
+    def check_duplicate(expected)
+      if @options[:cmd_tweaked] =~ /--#{expected.values[:long]}/ &&
+         @options[:cmd_tweaked] =~ /-#{expected.values[:short]}/
+        fail 'You cannot combine both long and short versions of an option!'
+      end
+    end
+
+    # Given a name, will return the object if the matching Option exists.
     def find_opt_by_name(name)
-      @command_options.each do |opt|
-        return true if opt.name == name
+      @expected_options.each do |opt|
+        return opt if opt.name == name
       end
       false
     end
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -1,5 +1,5 @@
-# rubocop:disable LineLength
 require 'spec_helper'
+require 'yaml'
 
 describe Optioneer do
   subject { Optioneer::Optioneer.new }
@@ -7,11 +7,39 @@ describe Optioneer do
   # clear the current ARGV and add our own test cases
   before(:each) do
     ARGV.clear
-    @cmd = %w(-r --quiet action)
+    # this should return 4 switches (2 with options) and one action
+    @cmd = %w(-r --quiet --switch=option -d directory action)
     (ARGV << @cmd).flatten!
+    # create specs to validate these
+    subject.add(:one, short: 'r', long: 'recurse')
+    subject.add(:two, short: 'q', long: 'quiet')
+    subject.add(:three, short: 's', long: 'switch', arg: 'OPTION')
+    subject.add(:four, short: 'd', arg: 'DIRECTORY')
+    # puts subject.to_yaml
   end
 
   it 'stores the given command line when created' do
     expect(subject.cmdline).to eq @cmd
+  end
+
+  context 'when parsing the command line' do
+    it 'returns the correct number of switches (long or short)' do
+      expect(subject.parse).to eq 4
+    end
+    it 'returns any specified arguments to switches' do
+      subject.parse
+      expect(subject[:three].values[:arg]).to eq 'option'
+      expect(subject[:four].values[:arg]).to eq 'directory'
+    end
+    it 'will not allow both the short and long form' do
+      pending 'to write'
+    end
+    it 'will only allow registered options' do
+      pending 'to write'
+    end
+    it 'returns the action specified' do
+      subject.parse
+      expect(subject.action).to eq 'action'
+    end
   end
 end
